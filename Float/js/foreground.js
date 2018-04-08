ea9cffa5440f7ba9ec70c9936020e360111627ea
Snoof <s3507371@student.rmit.edu.au>
2018-04-08 13:50:20 +1000

    Added Obstacle module

--- Float/js/foreground.js
+++ Float/js/foreground.js
@@ -3,17 +3,23 @@
 function Foreground(width, height) {
     this.base = height - (height / 3);
     this.x = width;
+    // y coordinate is the ground of the foreground
+    // use this point for where player stands and obstacles sit
     this.y = this.base;
+    // create new obstacle 'objects'
+    var obs = new Obstacle(this.x, this.y);
     // function to update foreground (make it move)
     this.update = function () {
         this.x -= gameSpeed;
     }
     // function to draw the foreground in it's current coordinates
     this.show = function () {
-        fill(150,90,80);
+        fill(150, 90, 80);
+        // this rect is the 'ground'
         rect(this.x, this.y, width * 2, height);
-        ellipse(this.x + (width / 2), this.y + 20, 200, 100);
-        ellipse(this.x + (width * 1.5), this.y + 20, 100, 100);
+        // show obstacles on foreground
+        obs.show();
+        obs.update();
     }
     this.offscreen = function () {
         return this.x < -(width);
---
+++ Float/js/obstacle.js
@@ -0,0 +1,18 @@
+﻿
+
+// function to create an obstacle 'object'
+// that takes x and y coordinates as parameters
+function Obstacle(x, y) {
+    this.x = x + 100;
+    this.y = y;
+
+    // function to show the obstacle
+    this.show = function () {
+        ellipse(this.x, this.y + 20, 100, 100);
+        ellipse(this.x + (width / 2), this.y + 20, 200, 100);
+    }
+    // function to update obstacle coordinates
+    this.update = function (){
+
+    }
+}
