c026f2acbb8f1a7204772602e4b0345c988bb60c
Snoof <s3507371@student.rmit.edu.au>
2018-04-10 11:24:20 +1000

    Added player functionality and a template for loading the player image

--- Float/js/main.js
+++ Float/js/main.js
@@ -1,35 +1,56 @@
-﻿
-// initialise variables here
-var gameSpeed = 1;
+﻿// initialise global variables here
+var canvas;
+var gameSpeed = 2;
 var bground;
 var fground = [];
-var groundHeight;
+var ground;
+var gravity = .2;
 var player;
+var playerImg;
 
+function preload() {
+    // load player image here
+    playerImg = loadImage("https://i.imgur.com/Quggv8b.jpg");
+}
 // This function does all the necessary on-load initialisation stuff
 function setup() {
     // Creates a canvas 600 width 400 height
     createCanvas(600, 400);
+    playerImg.resize(0, 50);
     ground = height - (height / 3);
     bground = new Background();
     fground.push(new Foreground(0, width * 2, height));
-    //add new player object
-    //fground.push(new Foreground(width*2, width*4, height));
+    player = new Player();
 }
 
 // This is the game loop. Anything put in here is looped over continuously
 function draw() {
     // draws the background image from background.js
     bground.show();
+
     for (var i = 0; i < fground.length; ++i) {
         fground[i].show();
         fground[i].update();
     }
+
     if (fground[0].needsNew()) {
-        fground.push(new Foreground(width, width*2, height));
-    }
-    else if (fground[0].offscreen()) {
+        fground.push(new Foreground(width, width * 2, height));
+    } else if (fground[0].offscreen()) {
         fground.splice(0, 1);
     }
+    player.update();
+    player.show();
 
+}
+
+function keyTyped() {
+    if (key == 'w') {
+        player.jump();
+    }
+    if (key == 'a') {
+        player.moveLeft();
+    }
+    if (key == 'd') {
+        player.moveRight();
+    }
 }
--- Float/js/player.js
+++ Float/js/player.js
@@ -1,15 +1,50 @@
-﻿
-var playerHeight = 50;
+﻿var playerHeight = 50;
+var startX = 100;
 
+// function to create the player
 function Player() {
-    this.x = 100;
+    this.x = startX;
     this.y = ground;
-
+    this.jumpH = -8;
+    this.velocity = 0;
+    this.jumping = false;
+    this.img = playerImg;
+    // function to show the player
     this.show = function () {
-
+        // fill(color(255, 50, 50));
+        // rect(this.x, this.y - playerHeight, 20, playerHeight);
+        image(this.img, this.x, this.y - 50);
+    }
+    // function to control the players movement
+    this.update = function () {
+        this.velocity += gravity;
+        this.velocity *= .95;
+        this.y += this.velocity;
+        if (this.y > ground) {
+            this.y = ground;
+            this.velocity = 0;
+        }
+        if (this.y < 0) {
+            this.y = ground;
+            this.velocity = 0;
+        }
     }
 
-    this.update = function () {
-        
+    this.jump = function () {
+        // remove this if to make floating dog
+        if (this.y == ground) {
+            this.velocity += this.jumpH;
+        }
+    }
+
+    this.moveLeft = function () {
+        if (this.x > 0) {
+            this.x -= 10;
+        }
+    }
+    this.moveRight = function () {
+        if (this.x < 200) {
+            this.x += 10;
+        }
     }
 }
